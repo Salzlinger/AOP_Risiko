d509a8e0d8f6c31e4cef840ae5573bea4b9046fd
bzudemi <86731025+bzudemi@users.noreply.github.com>
2021-08-03 15:11:52 +0200

    UpdateGUI

--- AOP_Risiko/src/Risiko/RisikoGUI.java
+++ AOP_Risiko/src/Risiko/RisikoGUI.java
@@ -2,10 +2,20 @@ package Risiko;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
+import java.awt.RenderingHints;
+import java.awt.Shape;
 import java.awt.event.ActionListener;
+import java.awt.geom.RoundRectangle2D;
 
+import javax.swing.AbstractButton;
+import javax.swing.Box;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,6 +23,9 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.basic.BasicButtonUI;
+import javax.swing.plaf.basic.BasicTextFieldUI;
 
 public class RisikoGUI extends JFrame {
 	
@@ -20,8 +33,6 @@ public class RisikoGUI extends JFrame {
 
 	private JPanel mainPanel;
 
-	private GridLayout mainLayout;
-	
 	private JButton startButton;
 	private JButton exitButton;
 	
@@ -49,14 +60,24 @@ public class RisikoGUI extends JFrame {
 	{
 		// Panel
 		mainPanel = new JPanel();
+		mainPanel.setBackground(new Color(0xcccccc));
 		
 		// Layout
-		mainLayout = new GridLayout(2, 1, 0, 10);
-		mainPanel.setLayout(mainLayout);
+		mainPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		mainPanel.setBorder(new EmptyBorder(100, 80, 40, 80));
 		
 		// Buttons
-		startButton = new JButton("Neues Spiel");
+		startButton = new JButton("Starte Spiel");
+		startButton.setFont(new Font("Calibri", Font.PLAIN, 25));
+		startButton.setBackground(new Color(0x2dce98));
+		startButton.setForeground(Color.white);
+		startButton.setUI(new ButtonDesign());
+		
 		exitButton = new JButton("Beenden");
+		exitButton.setFont(new Font("Calibri", Font.PLAIN, 25));
+		exitButton.setBackground(new Color(0x2dce98));
+		exitButton.setForeground(Color.white);
+		exitButton.setUI(new ButtonDesign());
 		
 		// Commands
 		startButton.setActionCommand(startButtonName);
@@ -64,6 +85,7 @@ public class RisikoGUI extends JFrame {
 		
 		// Buttons hinzufügen
 		mainPanel.add(startButton);
+		mainPanel.add(Box.createRigidArea(new Dimension(50, 20)));
 		mainPanel.add(exitButton);
 		
 		return mainPanel;
@@ -75,14 +97,15 @@ public class RisikoGUI extends JFrame {
 		startButton.addActionListener(e);
 		exitButton.addActionListener(e);
 	}
+
 }
+
 
 class SpielerAnzahl extends JDialog {
 	
 	private static final long serialVersionUID = 1L;
 	
 	private JPanel spielerAnzahlPanel;
-	private GridLayout spielerAnzahlLayout;
 	private JLabel spielerAnzahlLabel;
 	
 	private JButton dreiSpielerBtn;
@@ -112,16 +135,38 @@ class SpielerAnzahl extends JDialog {
 	private JPanel spielerAnzahlPanel()
 	{
 		spielerAnzahlPanel = new JPanel();
+		spielerAnzahlPanel.setBackground(new Color(0xcccccc));
 		
-		spielerAnzahlLayout = new GridLayout(5, 1, 5, 5);
-		spielerAnzahlPanel.setLayout(spielerAnzahlLayout);
+		// Layout
+		spielerAnzahlPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		spielerAnzahlPanel.setBorder(new EmptyBorder(20, 80, 40, 80));
 		
 		spielerAnzahlLabel = new JLabel("Anzahl der Spieler:");
+		spielerAnzahlLabel.setFont(new Font("Calibri", Font.PLAIN, 25));
 		
 		dreiSpielerBtn = new JButton("Drei");
+		dreiSpielerBtn.setFont(new Font("Calibri", Font.PLAIN, 25));
+		dreiSpielerBtn.setBackground(new Color(0x2dce98));
+		dreiSpielerBtn.setForeground(Color.white);
+		dreiSpielerBtn.setUI(new ButtonDesign());
+		
 		vierSpielerBtn = new JButton("Vier");
+		vierSpielerBtn.setFont(new Font("Calibri", Font.PLAIN, 25));
+		vierSpielerBtn.setBackground(new Color(0x2dce98));
+		vierSpielerBtn.setForeground(Color.white);
+		vierSpielerBtn.setUI(new ButtonDesign());
+		
 		fuenfSpielerBtn = new JButton("Fünf");
-		zurueckBtn = new JButton("Zurück");
+		fuenfSpielerBtn.setFont(new Font("Calibri", Font.PLAIN, 25));
+		fuenfSpielerBtn.setBackground(new Color(0x2dce98));
+		fuenfSpielerBtn.setForeground(Color.white);
+		fuenfSpielerBtn.setUI(new ButtonDesign());
+		
+	  	zurueckBtn = new JButton("Zurück");
+		zurueckBtn.setFont(new Font("Calibri", Font.PLAIN, 25));
+		zurueckBtn.setBackground(new Color(0x2dce98));
+		zurueckBtn.setForeground(Color.white);
+		zurueckBtn.setUI(new ButtonDesign());
 		
 		dreiSpielerBtn.setActionCommand(dreiSpielerBtnName);
 		vierSpielerBtn.setActionCommand(vierSpielerBtnName);
@@ -129,9 +174,11 @@ class SpielerAnzahl extends JDialog {
 		zurueckBtn.setActionCommand(zurueckBtnName);
 		
 		spielerAnzahlPanel.add(spielerAnzahlLabel);
+		spielerAnzahlPanel.add(Box.createRigidArea(new Dimension(50, 20)));
 		spielerAnzahlPanel.add(dreiSpielerBtn);
 		spielerAnzahlPanel.add(vierSpielerBtn);
 		spielerAnzahlPanel.add(fuenfSpielerBtn);
+		spielerAnzahlPanel.add(Box.createRigidArea(new Dimension(50, 40)));
 		spielerAnzahlPanel.add(zurueckBtn);
 		
 		return spielerAnzahlPanel;
@@ -180,8 +227,8 @@ class SpielereinstellungGUI extends JDialog {
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setResizable(false);
 		
-		mainLayout = new GridLayout(1, 1, 5, 5);
-		setLayout(mainLayout);
+//		mainLayout = new GridLayout(1, 1, 5, 5);
+	//	setLayout(mainLayout);
 		
 		add(spielerNamePanel());
 		
@@ -192,18 +239,19 @@ class SpielereinstellungGUI extends JDialog {
 	private JPanel spielerNamePanel()
 	{
 		spielerNamePanel = new JPanel();
+		spielerNamePanel.setBackground(new Color(0xcccccc));
 		
-		spielerNamePanel.setPreferredSize(new Dimension(400, spielerZahl * 80 + 40));
+		spielerNamePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		spielerNamePanel.setBorder(new EmptyBorder(20, 80, 10, 80));
 		
-		spielerLayout = new GridLayout(spielerZahl + 2, 1, 5, 5);
-		spielerNamePanel.setLayout(spielerLayout);
+		spieler1TextField = new JTextFieldDesign(15);
+		spieler1TextField.setBackground(new Color(0x0b5394));
 		
-		spieler1TextField = new JTextField("Peter");
-		spieler1TextField.setBackground(Color.BLUE);
-		spieler2TextField = new JTextField("Hans");
-		spieler2TextField.setBackground(Color.RED);
-		spieler3TextField = new JTextField("Werner");
-		spieler3TextField.setBackground(Color.YELLOW);
+		spieler2TextField = new JTextFieldDesign(15);
+		spieler2TextField.setBackground(new Color(0xcc0000));
+		
+		spieler3TextField = new JTextFieldDesign(15);
+		spieler3TextField.setBackground(new Color(0xffff00));
 		
 		spielerNamePanel.add(spieler1TextField);
 		spielerNamePanel.add(spieler2TextField);
@@ -211,26 +259,41 @@ class SpielereinstellungGUI extends JDialog {
 		
 		if (spielerZahl == 4)
 		{
-			spieler4TextField = new JTextField("Joachim");
-			spieler4TextField.setBackground(Color.GREEN);
+			spieler4TextField = new JTextFieldDesign(15);
+			spieler4TextField.setBackground(new Color(0x2dce98));
 			spielerNamePanel.add(spieler4TextField);
 		}
 		if (spielerZahl == 5)
 		{
-			spieler4TextField = new JTextField("Joachim");
-			spieler4TextField.setBackground(Color.GREEN);
-			spieler5TextField = new JTextField("Klaus");
-			spieler5TextField.setBackground(Color.WHITE);
+			spieler4TextField = new JTextFieldDesign(15);		
+			spieler4TextField.setBackground(new Color(0x2dce98));
+			spieler5TextField = new JTextFieldDesign(15);
+			spieler5TextField.setBackground(new Color(0xffffff));
+			
 			spielerNamePanel.add(spieler4TextField);
 			spielerNamePanel.add(spieler5TextField);
 		}
 		
+		spielerNamePanel.add(Box.createRigidArea(new Dimension(50, 40)));
+		
 		zurueckBtn = new JButton("Zurück");
+		zurueckBtn.setFont(new Font("Calibri", Font.PLAIN, 20));
+		zurueckBtn.setBackground(new Color(0x2dce98));
+		zurueckBtn.setForeground(Color.white);
+		zurueckBtn.setUI(new ButtonDesign());
 		zurueckBtn.setActionCommand(zurueckBtnName);
-		spielerNamePanel.add(zurueckBtn);
+		
 		startBtn = new JButton("Starte Spiel");
+		startBtn.setFont(new Font("Calibri", Font.PLAIN, 30));
+		startBtn.setBackground(new Color(0x2dce98));
+		startBtn.setForeground(Color.white);
+		startBtn.setUI(new ButtonDesign());
 		startBtn.setActionCommand(startBtnName);
+		
 		spielerNamePanel.add(startBtn);
+		spielerNamePanel.add(Box.createRigidArea(new Dimension(50, 40)));
+		spielerNamePanel.add(zurueckBtn);
+		
 		
 		return spielerNamePanel;
 	}
@@ -293,4 +356,56 @@ class Spielbrett extends JDialog {
 	        SwingUtilities.invokeLater(r);
 		
 	}
+}
+class ButtonDesign extends BasicButtonUI {
+	
+	@Override
+	public void installUI (JComponent c) {
+		super.installUI(c);
+		AbstractButton button = (AbstractButton) c;
+		button.setOpaque(false);
+		button.setBorder(new EmptyBorder(5, 15, 5, 15));
+	}
+	
+	@Override
+	public void paint (Graphics g, JComponent c) {
+		AbstractButton b = (AbstractButton) c;
+		paintBackground(g, b, b.getModel().isPressed() ? 2: 0);
+		super.paint(g, c);
+	}
+	
+	private void paintBackground(Graphics g, JComponent c, int yOffset) {
+		 Dimension size = c.getSize();
+	        Graphics2D g2 = (Graphics2D) g;
+	        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	        g.setColor(c.getBackground().darker());
+	        g.fillRoundRect(0, yOffset, size.width, size.height - yOffset, 10, 10);
+	        g.setColor(c.getBackground());
+	        g.fillRoundRect(0, yOffset, size.width, size.height + yOffset - 5, 10, 10);
+	}
+}
+
+class JTextFieldDesign extends JTextField {
+
+	private static final long serialVersionUID = 1L;
+	private Shape shape;
+    public JTextFieldDesign(int size) {
+        super(size);
+        setOpaque(false); // As suggested by @AVD in comment.
+    }
+    protected void paintComponent(Graphics g) {
+         g.setColor(getBackground());
+         g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+         super.paintComponent(g);
+    }
+    protected void paintBorder(Graphics g) {
+         g.setColor(getForeground());
+         g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+    }
+    public boolean contains(int x, int y) {
+         if (shape == null || !shape.getBounds().equals(getBounds())) {
+             shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+         }
+         return shape.contains(x, y);
+    }
 }
